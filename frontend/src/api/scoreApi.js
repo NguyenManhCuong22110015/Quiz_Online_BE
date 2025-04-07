c317378d9984f48fcc6db62816754c775307e713
Your Name <you@example.com>
2025-04-07 18:02:24 +0700

    push be

--- frontend/src/api/scoreApi.js
+++
@@ -1,6 +0,0 @@
-import { API } from "../utils/apiFunctions";
-
-export async function getTopPlayers(userId) {
-    const response = await API.get("api/quizzes/getByUserId?userId=" + userId);
-    return response.data;
-};
